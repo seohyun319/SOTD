5639d27e04b5310d9e7b760f3a7d12f0be9ec732
songfox00 <songfox00@sookmyung.ac.kr>
2021-11-07 07:07:58 +0900

    fortune 구현 완료

--- server/server.js
+++ server/server.js
@@ -8,13 +8,15 @@ const conf = JSON.parse(data);
 const mysql=require('mysql');
 const request=require('request');
 const { Router } = require('express');
+const schedule = require('node-schedule');
 
 const connection = mysql.createConnection({
     host: conf.host,
     user: conf.user,
     password: conf.password,
     port: conf.port,
-    database: conf.database
+    database: conf.database,
+    multipleStatements:true
 })
 
 connection.connect();
@@ -36,8 +38,42 @@ app.use((req, res, next) => {
     next();
 })
 
-var query=connection.query('SELECT * FROM FORTUNE', (err, rows, fields) => {
-    console.log(rows);
+const reset=schedule.scheduleJob('0 0 0 * * *', ()=>{
+    var i;
+    for(i=1;i<=365;i++){
+        var query=connection.query('UPDATE birth SET luck=(select luck from FORTUNE ORDER BY RAND() limit 1) WHERE id=?', i, (err, rows, fields)=>{
+            console.log("reset");
+        })
+    }
 })
+
+app.post('/info', (req,res)=>{   //생년월일 확인
+    
+})
+
+app.post('/info/fortune', (req, res)=>{ //생일 받아오기
+    var month=req.body.month;
+    var day=req.body.day;
+    var sql=[month, day];
+    var query=connection.query('SELECT category FROM FORTUNE WHERE luck=(SELECT luck FROM birth WHERE month=? AND day=?)',[4,1],(err, rows, fields)=>{
+        request.post('http://localhost:5000/info/fortunefood',{form:{    //category에서 가게 이름 추출하기 위해 전달
+            category: rows
+        }})
+        res.send(rows);
+    })
+})
+
+app.post('/info/fortunefood',(req,res)=>{
+    var category=req.body.category; //category column 값
+    for(var i=0;i<category.length;i++){
+        var cate=(category[i].category).split(', ');    //category를 , 기준으로 나누기
+        for(var j=0;j<cate.length;j++){
+            var query=connection.query('SELECT distinct title FROM '+ cate[j]+' ORDER BY RAND() limit 1',(err,rows,fields)=>{
+                res.send(rows);
+            })
+        }
+    }
+})
+
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
